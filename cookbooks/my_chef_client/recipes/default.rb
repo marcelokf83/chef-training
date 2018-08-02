fbc2b301c68a6635fd7442c3b9af35b655456377
marcelokf83 <marcelokf83@gmail.com>
2018-08-02 01:13:19 +0000

    Community Cookbook Lab

---
+++ cookbooks/my_chef_client/recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: my_chef_client
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+include_recipe 'chef-client::default'
---
+++ roles/base.rb
@@ -0,0 +1,3 @@
+name 'base'
+description 'Base Role'
+run_list 'recipe[my_chef_client]'
